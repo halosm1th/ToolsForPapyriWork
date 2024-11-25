89c505100c42bd23c33d0379cf2b6e88738d1967
halosm1th <16440148+halosm1th@users.noreply.github.com>
2024-11-24 22:38:33 -0600

    Added the code I used for the last project to this one

--- Papryi Checklist Items Exist in DB Verification/Program.cs
+++ Papryi Checklist Items Exist in DB Verification/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PapyriChecklistItems;
+using 
+class PapyriChecklistInfo
+{
+    public static void Main()
+    {
+        
+    }
+}
