373bc7726a02be01def7d018964b13018df392dc
halosm1th <16440148+halosm1th@users.noreply.github.com>
2024-11-24 23:00:16 -0600

    Big moment, just finished writing in the basic first class. This will be our searcher. Over time it will grow more complex, but for the moment we're testing the retrieve information aspect of it, and if it can find the search result table data and pull stuff from it. After this, the next necessity will be pulling the number of hits, checking to make sure that doesn't contain a zero, and then moving on from there.

--- Papryi Checklist Items Exist in DB Verification/Program.cs
+++ Papryi Checklist Items Exist in DB Verification/Program.cs
@@ -6,6 +6,43 @@ class PapyriChecklistInfo
 {
     public static void Main()
     {
+        var searcher = new PapyriSearcher();
+        searcher.BibliographySearch("Actenstücke", "", new() {"1887", "London", "Paris"});
+        Console.ReadLine();
+    }
+}
+
+class PapyriSearcher
+{
+    HtmlWeb RequestClient = new HtmlWeb();
+    private const string SearchURL = "https://papyri.info/bibliosearch?q=";
+
+
+    public List<string> BibliographySearches(List<string> titles, List<string> numbers, List<List<string>> otherDatas)
+    {
+        var results = new List<string>() { };
+        for(int i=0; i <titles.Count; i++)
+        {
+            var result = BibliographySearch(titles[i], numbers[i], otherDatas[i]);
+            results.Add(result);
+        }
+
+        return results;
+    }
+    
+    public string BibliographySearch(string title, string number, List<string> otherData)
+    {
+        var result = "";
+        var finalSearchURL = SearchURL + title + "+" + number;
+        var page = RequestClient.Load(finalSearchURL);
+
+        var searchResult = page.DocumentNode.SelectNodes("//td/").First();
+
+        foreach (var node in searchResult.ChildNodes)
+        {
+            Console.WriteLine(node.InnerText);
+        }
         
+        return result;
     }
 }
